ca49c6f7f02b91997d993124122c61e8d455a8ef
HP-Laptop <HP-Laptop@DESKTOP-6OV483N>
2022-06-27 14:27:18 +0530

    Committe Java Project 2022

---
+++ src/Java_Lecture_01/Mahendra.java
@@ -0,0 +1,17 @@
+package Java_Lecture_01;
+
+public class Mahendra {
+	public void pqr() {
+		System.out.println("Hyy");System.out.println("Sachin");
+	}
+	
+public static void main(String[] args) {
+
+	Mahendra stats=new Mahendra();
+	stats.pqr();
+	
+}
+
+
+}
+ 
